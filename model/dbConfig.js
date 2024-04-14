6a38a10456ffaecd1f84d63537d7838364b2bffd
Nathan-bit <gabiamsamuelnathan@gmail.com>
2024-04-14 10:36:10 +0100

    Update database configuration to use environment variables
    The code diff includes updates to the `.env` file, `model/dbConfig.js`, and `model/model.js` files to use environment variables for database configuration. The `.

--- model/dbConfig.js
+++ model/dbConfig.js
@@ -1,11 +1,12 @@
 // dbConfig.js
+require('dotenv').config();
 
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'data'
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME
 });
 
 connection.connect(err => {
--- model/model.js
+++ model/model.js
@@ -2,10 +2,10 @@
 //model/model.js
 const { Sequelize, DataTypes } = require('sequelize');
 // Replace 'database_name', 'username', 'password', and 'host' with your MySQL database credentials
-const sequelize = new Sequelize('data', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306,
+const sequelize = new Sequelize(process.env.DATABASE_NAME,process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+  host: process.env.DATABASE_HOST,
+  dialect: process.env.DATABASE_DIALECT,
+  port: process.env.DATABASE_PORT,
 });
 
 // Define the  model
